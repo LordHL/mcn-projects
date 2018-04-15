349969851ac92e6a347f149ed156d02c8d547e87
dinghao <dinghao@hiekn.com>
2018-04-16 00:46:42 +0800

    提取默认参数，精简项目配置，并添加初始化器，监听器以及环境后置处理器

---
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnContextInitializer.java
@@ -0,0 +1,15 @@
+package com.hiekn.boot.autoconfigure.context;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 9)
+public class McnContextInitializer implements ApplicationContextInitializer{
+
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+
+    }
+}
---
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnPropertiesPostProcessor.java
@@ -0,0 +1,46 @@
+package com.hiekn.boot.autoconfigure.context;
+
+import com.google.common.collect.Maps;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+public class McnPropertiesPostProcessor implements EnvironmentPostProcessor,Ordered {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        MutablePropertySources propertySources = environment.getPropertySources();
+        Map<String, Object> source = Maps.newHashMap();
+        String name = application.getMainApplicationClass().getName();
+        String pkg = name.substring(0,name.lastIndexOf("."));
+        source.put("jersey.swagger.base-package",pkg);
+        MapPropertySource mapPropertySource = new MapPropertySource("mcn-map",source);
+        try {
+            System.out.println(this.getClass().getResource("").getPath());
+            String path=this.getClass().getResource("").getPath();
+            path=path.replace("file:", "jar:file:");
+            path=path.replace("com/hiekn/boot/autoconfigure/context", "META-INF");
+            Properties properties = PropertiesLoaderUtils.loadProperties(new UrlResource(path+"mcn.properties"));
+            PropertiesPropertySource propertySource = new PropertiesPropertySource("mcn-prop",properties);
+            propertySources.addLast(propertySource);
+        } catch (IOException e) {
+        }
+        propertySources.addLast(mapPropertySource);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE + 11;
+    }
+
+}
