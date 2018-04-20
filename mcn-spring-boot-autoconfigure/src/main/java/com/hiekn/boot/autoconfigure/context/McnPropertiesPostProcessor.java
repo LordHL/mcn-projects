ae38cc2a1349a41ffb686a79c5944e817450d1c9
DingHao <dh.hiekn@gmail.com>
2018-04-21 00:04:13 +0800

    添加全局配置文件mcn-global-[dev/prod].properties解析，例如可以定义全局数据源

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnPropertiesPostProcessor.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnPropertiesPostProcessor.java
@@ -10,6 +10,7 @@ import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.core.io.support.ResourcePropertySource;
 
 import java.io.IOException;
 import java.util.Map;
@@ -22,35 +23,32 @@ public class McnPropertiesPostProcessor implements EnvironmentPostProcessor,Orde
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         MutablePropertySources propertySources = environment.getPropertySources();
         //add map config
-        Map<String, Object> source = Maps.newHashMap();
+        Map<String, Object> mapProp = Maps.newHashMap();
         String name = application.getMainApplicationClass().getName();
         String pkg = name.substring(0,name.lastIndexOf("."));
-        source.put("jersey.swagger.base-package",pkg);
-        MapPropertySource mapPropertySource = new MapPropertySource("mcn-map",source);
+        mapProp.put("jersey.swagger.base-package",pkg);
+        propertySources.addLast(new MapPropertySource("mcn-map",mapProp));
+
         try {
             String[] activeProfiles = environment.getActiveProfiles();
             //add global config
-            Properties globalProperties = new Properties();
             String globalConfigName = "mcn-global";
             if(Objects.nonNull(activeProfiles)){
                 globalConfigName += "-"+activeProfiles[0];
             }
-            globalProperties.load(this.getClass().getClassLoader().getResourceAsStream(globalConfigName+".properties"));
-            PropertiesPropertySource globalPropertySource = new PropertiesPropertySource("mcn-global",globalProperties);
-            propertySources.addLast(globalPropertySource);
+            propertySources.addLast(new ResourcePropertySource("mcn-global","classpath:"+globalConfigName+".properties"));
         } catch (Exception e) {
+
         }
         try {
             //add mcn default config
             String path=this.getClass().getResource("").getPath();
             path=path.replace("file:", "jar:file:");
             path=path.replace("com/hiekn/boot/autoconfigure/context", "META-INF");
-            Properties properties = PropertiesLoaderUtils.loadProperties(new UrlResource(path+"mcn.properties"));
-            PropertiesPropertySource propertySource = new PropertiesPropertySource("mcn-prop",properties);
-            propertySources.addLast(propertySource);
+            propertySources.addLast(new PropertiesPropertySource("mcn-prop",PropertiesLoaderUtils.loadProperties(new UrlResource(path+"mcn.properties"))));
         } catch (IOException e) {
+
         }
-        propertySources.addLast(mapPropertySource);
     }
 
     @Override
