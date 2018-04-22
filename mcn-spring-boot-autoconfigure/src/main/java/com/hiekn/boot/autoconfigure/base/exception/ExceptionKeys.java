46400bdac58d4465cce970e9dc0ecc0537431499
DingHao <dh.hiekn@gmail.com>
2018-04-22 16:06:51 +0800

    重定义错误码

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/exception/ExceptionKeys.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/exception/ExceptionKeys.java
@@ -1,28 +1,27 @@
 package com.hiekn.boot.autoconfigure.base.exception;
-
+/*
+    3xxxx:通用错误码定义
+    5xxxx:业务相关错误码定义
+    7xxxx:未知错误码
+    8xxxx:Http相关错误码定义
+    9xxxx:统一错误码及第三方服务错误码定义
+ */
 public interface ExceptionKeys {
-    //3xxxx:通用错误码定义
-    //5xxxx:业务相关错误码定义
-    //7xxxx:未知错误码
-    //8xxxx:Http相关错误码定义
-    //9xxxx:统一错误码及第三方服务错误码定义
 
     Integer PARAM_PARSE_ERROR = 30001;
     Integer JSON_PARSE_ERROR = 30002;
+    Integer INIT_ERROR = 30003;
+    Integer VERIFY_CODE_ERROR = 30004;
+    Integer USER_EXIST_ERROR = 30005;
+    Integer USER_NOT_FOUND_ERROR = 30006;
+    Integer USER_PWD_ERROR = 30007;
+    Integer PWD_ERROR = 30008;
+    Integer GET_MOBILE_CODE_ERROR = 30009;
+    Integer VERIFY_MOBILE_CODE_ERROR = 30010;
+    Integer UPLOAD_ERROR = 30011;
 
-    Integer EXISTED_ERROR = 50001;
-    Integer USER_NOT_FOUND_ERROR = 50002;
-    Integer PWD_ERROR = 50003;
-    Integer GET_CODE_ERROR = 50004;
-    Integer VERIFY_MOBILE_CODE_ERROR = 50005;
-    Integer VERIFY_CODE_ERROR = 50006;
-    Integer USER_EXIST_ERROR = 50007;
-    Integer UN_LOGIN_ERROR = 50008;
-    Integer USER_PWD_ERROR = 50009;
-    Integer INIT_ERROR = 50010;
-    Integer UPLOAD_ERROR = 50011;
-    Integer AUTHENTICATION_ERROR = 50012;
-    Integer TOKEN_CREATE_ERROR = 50013;
+    Integer UN_LOGIN_ERROR = 50001;
+    Integer AUTHENTICATION_ERROR = 50002;
 
     Integer UNKNOWN_ERROR = 70001;
 
