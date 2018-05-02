220339153297175fc07fea308088607544983d1d
DingHao <dh.hiekn@gmail.com>
2018-05-02 22:38:41 +0800

    添加多个事件监听（泛型过滤），使用推荐监听器

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnApplicationListener.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnApplicationListener.java
@@ -1,14 +1,26 @@
 package com.hiekn.boot.autoconfigure.context;
 
 import com.hiekn.boot.autoconfigure.base.util.BeanUtils;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.GenericApplicationListener;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.core.ResolvableType;
 
-@Order(Ordered.HIGHEST_PRECEDENCE + 9)
-public class McnApplicationListener implements ApplicationListener {
+public class McnApplicationListener implements GenericApplicationListener {
+
+    private static Class<?>[] EVENT_TYPES = { ApplicationStartingEvent.class,
+            ApplicationEnvironmentPreparedEvent.class, ApplicationPreparedEvent.class,
+            ContextClosedEvent.class, ApplicationFailedEvent.class };
+
+    private static Class<?>[] SOURCE_TYPES = { SpringApplication.class,
+            ApplicationContext.class };
 
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
@@ -16,5 +28,31 @@ public class McnApplicationListener implements ApplicationListener {
             ApplicationEnvironmentPreparedEvent event = (ApplicationEnvironmentPreparedEvent) applicationEvent;
             event.getSpringApplication().addListeners(new BeanUtils());
         }
+    }
+
+    @Override
+    public boolean supportsEventType(ResolvableType resolvableType) {
+        return isAssignableFrom(resolvableType.getRawClass(), EVENT_TYPES);
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> sourceType) {
+        return isAssignableFrom(sourceType, SOURCE_TYPES);
+    }
+
+    private boolean isAssignableFrom(Class<?> type, Class<?>... supportedTypes) {
+        if (type != null) {
+            for (Class<?> supportedType : supportedTypes) {
+                if (supportedType.isAssignableFrom(type)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE + 9;
     }
 }
