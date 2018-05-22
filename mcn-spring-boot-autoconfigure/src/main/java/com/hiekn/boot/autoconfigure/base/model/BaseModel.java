926fcea8e99010debfc1425097b23191dde524e5
kse-music <dh.hiekn@gmail.com>
2018-05-22 16:07:03 +0800

    add baseModel

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/model/BaseModel.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/model/BaseModel.java
@@ -1,10 +1,13 @@
 package com.hiekn.boot.autoconfigure.base.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class BaseModel {
-
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8" )
     private Date createTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8" )
     private Date updateTime;
 
     public Date getCreateTime() {
