4ea9964e2f4c5a9c517c8576d483f4f6ab69a937
kse-music <dh.hiekn@gmail.com>
2018-06-13 17:02:50 +0800

    重新定义错误码源

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/util/ErrorMsgUtil.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/util/ErrorMsgUtil.java
@@ -1,24 +1,41 @@
 package com.hiekn.boot.autoconfigure.base.util;
 
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
 
 import java.io.IOException;
 
 public class ErrorMsgUtil {
 
-    private static PropertiesPropertySource propertiesPropertySource;
+    private static MutablePropertySources mutablePropertySources;
 
     static {
+        mutablePropertySources = new MutablePropertySources();
+        ResourcePropertySource resourcePropertySource;
         try {
-            propertiesPropertySource = new PropertiesPropertySource("errMsg",PropertiesLoaderUtils.loadAllProperties("mcn-error-msg.properties"));
+            resourcePropertySource = new ResourcePropertySource("error-msg.properties");
+            mutablePropertySources.addLast(resourcePropertySource);
+        } catch (IOException e) {
+
+        }
+        try {
+            resourcePropertySource = new ResourcePropertySource("mcn-error-msg.properties");
+            mutablePropertySources.addLast(resourcePropertySource);
         } catch (IOException e) {
 
         }
     }
 
     public static String getErrMsg(Integer code){
-        return propertiesPropertySource.getProperty(code.toString()).toString();
+        for (PropertySource<?> propertySource : mutablePropertySources) {
+            Object value = propertySource.getProperty(code.toString());
+            if (value != null) {
+                return value.toString();
+            }
+        }
+        return null;
     }
+
 
 }
