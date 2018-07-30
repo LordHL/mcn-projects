d57a1ccfabfb47a84ee903edca46527516994cf5
kse-music <dh.hiekn@gmail.com>
2018-07-30 11:17:32 +0800

    添加手机参数校验器、移除分页类至基础模型，默认不打印mybatis查询语句

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/model/BaseModel.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/model/BaseModel.java
@@ -1,14 +1,28 @@
 package com.hiekn.boot.autoconfigure.base.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiParam;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.QueryParam;
 import java.util.Date;
+import java.util.Objects;
 
 public class BaseModel {
 
+    @ApiParam("当前页，默认1")
+    @DefaultValue("1")
+    @Min(1)
+    @QueryParam("pageNo")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Integer pageNo;
 
+    @ApiParam("每页数，默认10")
+    @DefaultValue("10")
+    @Max(50)
+    @QueryParam("pageSize")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Integer pageSize;
 
@@ -17,7 +31,10 @@ public class BaseModel {
     private transient Date updateTime;
 
     public Integer getPageNo() {
-        return pageNo;
+        if(Objects.isNull(pageNo)){//此处不能用三目表达式，猜测利用反射获取的值不能带逻辑判断？
+            return pageNo;
+        }
+        return (pageNo - 1) * pageSize;
     }
 
     public void setPageNo(Integer pageNo) {
