0d674b7f1f665c8d9ea9a1055043a9081710625d
kse-music <dh.hiekn@gmail.com>
2018-11-30 11:24:20 +0800

    1.5.18

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/exception/handler/JwtAuthenticationEntryPoint.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/base/exception/handler/JwtAuthenticationEntryPoint.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtAuthenticationEntryPoint extends AbstractExceptionHandler implements AuthenticationEntryPoint {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private ObjectMapper mapper;
 
