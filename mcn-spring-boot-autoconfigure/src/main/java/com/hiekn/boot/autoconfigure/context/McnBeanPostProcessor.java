9af45df9541edca3e05cbf628d76a691fd9bff18
kse-music <dh.hiekn@gmail.com>
2019-01-03 18:08:41 +0800

    反射autowired

--- mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnBeanPostProcessor.java
+++ mcn-spring-boot-autoconfigure/src/main/java/com/hiekn/boot/autoconfigure/context/McnBeanPostProcessor.java
@@ -2,14 +2,11 @@ package com.hiekn.boot.autoconfigure.context;
 
 import com.hiekn.boot.autoconfigure.base.service.Autowired2;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.annotation.Annotation;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -21,17 +18,13 @@ public class McnBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         if(bean instanceof AutowiredAnnotationBeanPostProcessor){
             AutowiredAnnotationBeanPostProcessor abf = (AutowiredAnnotationBeanPostProcessor) bean;
-            Set<Class<? extends Annotation>> autowiredAnnotationTypes = new LinkedHashSet<>(5);
-            autowiredAnnotationTypes.add(Autowired.class);
-            autowiredAnnotationTypes.add(Value.class);
-            autowiredAnnotationTypes.add(Autowired2.class);
-            try {
-                autowiredAnnotationTypes.add((Class<? extends Annotation>) ClassUtils.forName("javax.inject.Inject", AutowiredAnnotationBeanPostProcessor.class.getClassLoader()));
-            }
-            catch (ClassNotFoundException ex) {
-                // JSR-330 API not available - simply skip.
-            }
-            abf.setAutowiredAnnotationTypes(autowiredAnnotationTypes);
+            ReflectionUtils.doWithLocalFields(abf.getClass(),(f) -> {
+                if(f.getName().equals("autowiredAnnotationTypes")){
+                    ReflectionUtils.makeAccessible(f);
+                    Set<Class<? extends Annotation>> autowiredAnnotationTypes = (Set<Class<? extends Annotation>>)f.get(abf);
+                    autowiredAnnotationTypes.add(Autowired2.class);
+                }
+            });
         }
         return bean;
     }
